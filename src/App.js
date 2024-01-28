f31c65819de41ebe10ccf8dbd0f6eab30588ebe5
Gayathri Sankar <gayathri.sankar03@gmail.com>
2024-01-28 23:40:44 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react'
+import {AiOutlinePlus} from 'react-icons/ai'
+import Todo from './Todo'
+
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#aa4b6b] to-[#3b8d99]`,
+  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
+}
+
+
+function App() {
+  const [todos, setTodos] = useState(['Learn React', 'Grind LeetCode'])
+  return (
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h3 className={style.heading}>Todo App</h3>
+        <form className={style.form}>
+          <input className={style.input} type="text" placeholder="Add Todo"/>
+          <button className={style.button}><AiOutlinePlus size = {30} /></button>         
+        </form>
+        <ul>
+          {todos.map((todo, index)=> (
+            <Todo key={index} todo={todo}/>
+          ))}
+          
+        </ul>
+        <p className={style.count}>You have 2 todos</p>
+      </div>
+    </div>
+  );
+}
+  
+
+export default App;
