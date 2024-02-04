6d94709c0834886b455c5586bbaf4246f07e1efc
Gayathri Sankar <gayathri.sankar03@gmail.com>
2024-02-04 23:01:03 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import {AiOutlinePlus} from 'react-icons/ai'
 import Todo from './Todo'
 import {db} from './firebase'
